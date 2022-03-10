9aa05169400937be9ae22960df7691d385d53952
Rupesh <patrarupesh9157@gmail.com>
2022-03-10 23:14:44 +0530

    template integration

--- client/urls.py
+++ client/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.index,name='cindex'),
+    path('about',views.about,name='about'),
+    path('codes',views.codes,name='codes'),
+    path('contact',views.contact,name='contact'),
+    path('treatments',views.treatments,name='treatments'),
+
 ]
--- client/views.py
+++ client/views.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('index Page')
+    return render(request,'clientindex.html')
+def about(request):
+    return render(request,'about.html')
+def codes(request):
+    return render(request,'codes.html')
+def contact(request):
+    return render(request,'contact.html')
+def header(request):
+    return render(request,'header.html')
+def treatments(request):
+    return render(request,'treatments.html')
