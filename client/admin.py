ca4c3432ee167cbb0dc44fcb2e89824daea32597
Rohit <purbiyarohit123@gmail.com>
2022-03-22 16:29:44 +0530

    22-03-2022,Rohit

--- client/admin.py
+++ client/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(ClientUser)
-admin.site.register(Inquiry)
-admin.site.register(BookingTest)
+# admin.site.register(ClientUser)
+# admin.site.register(Inquiry)
+# admin.site.register(BookingTest)
+
+@admin.register(ClientUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['fname','email','mobile','aadhar','address']
+    
+@admin.register(Inquiry)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['fullname','email','mobile']
+    
+@admin.register(BookingTest)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['client','test','book_time','pay_id']
